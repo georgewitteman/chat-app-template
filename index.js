d5f2f1ed47e6fc4a30ffd5b21ed7f13af9511882
George Witteman <georgewitteman@gmail.com>
2017-03-28 17:27:13 -0400

    Fix a bug and make the code easier to understand

--- index.js
+++ index.js
@@ -2,29 +2,32 @@
 
 // Our required libraries and global variables
 var express = require('express');
+var port = process.env.PORT || 3000;
+
+// Create our server
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
 
-// Any files in the ./public directory served statically
-// For example: public/index.html gets served as
-//              http://localhost:3000/index.html
+// Files in the `public/` directory served statically
 app.use(express.static('public'));
 
-// Listen for socket.io connections
-io.on('connection', function(socket){
+// When a user goes to the root url send them to index.html
+app.get('/', function redirectHomepage(req, res) {
+  res.redirect(301, '/index.html');
+});
 
+// Listen for socket.io connections
+io.on('connection', function listenForIOConnections(socket){
   // When a user types in a message...
-  socket.on('chat message', function(msg){
+  socket.on('chat message', function emitMessage(msg){
     // ...send that message to other connected users
     io.emit('chat message', msg);
   });
-
 });
 
 // Wait for users to come to our app
-http.listen(port, function(){
+http.listen(port, function startServer(){
   // Log our apps URL
   console.log('Listening on http://localhost:' + port);
 });
